49ee03e44c8e3e6154e2eb61de03219d5c8f1b6c
regiluze <regiluze@hotmail.com>
2015-02-08 11:56:49 +0100

    bugfix

--- server.go
+++ server.go
@@ -78,7 +78,7 @@ func (s *HttpServer) NotFound(w http.ResponseWriter, r *http.Request) {
 func (s *HttpServer) Deploy(context string, h RouteHandler) {
 	r := h.HandleRoutes(s.router, s.errorHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	http.Handle(fmt.Sprintf("/%s", context), r)
+	http.Handle(fmt.Sprintf("/%s/", context), r)
 	r.NotFoundHandler = http.HandlerFunc(s.NotFound)
 }
 
