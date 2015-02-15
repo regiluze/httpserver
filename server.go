cadf0338c10153cc9ede97ed8464da07a2d40380
regiluze <regiluze@hotmail.com>
2015-02-15 16:10:20 +0100

    refactoring

--- server.go
+++ server.go
@@ -19,35 +19,6 @@ const (
 	DeleteMethod        = "DELETE"
 )
 
-type Route struct {
-	Path       string
-	Method     string
-	HandleFunc http.HandlerFunc
-}
-
-func NewRoute(p string, m string, hf http.HandlerFunc) *Route {
-	r := &Route{Path: p, Method: m, HandleFunc: hf}
-	return r
-}
-
-func (r *Route) HasIncorrectHttpMethod(method string) bool {
-
-	if len(r.Method) != 0 && method != r.Method {
-		return true
-	}
-	return false
-
-}
-
-type ServerError struct {
-	Msg string
-}
-
-func NewError(msg string) *ServerError {
-	s := &ServerError{Msg: msg}
-	return s
-}
-
 type HttpServer struct {
 	port             string
 	address          string
@@ -60,10 +31,6 @@ func NewHttpServer(a string, p string) *HttpServer {
 	r := mux.NewRouter()
 	s := &HttpServer{router: r, address: a, port: p}
 	return s
-}
-
-type RouteHandler interface {
-	GetRoutes() []*Route
 }
 
 func (s *HttpServer) SetErrTemplate(t *template.Template) {
