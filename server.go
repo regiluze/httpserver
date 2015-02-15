6b6cc0293044a22f45bdd6a4decccdd4fb2a17fc
regiluze <regiluze@hotmail.com>
2015-02-15 16:16:56 +0100

    refactoring

--- server.go
+++ server.go
@@ -41,7 +41,7 @@ func (s *HttpServer) SetNotFoundTemplate(t *template.Template) {
 	s.notFoundTemplate = t
 }
 
-func (s *HttpServer) errorHandler(route *httpserver.Route) http.HandlerFunc {
+func (s *HttpServer) errorHandler(route *Route) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if recoverErr := recover(); recoverErr != nil {
@@ -88,4 +88,37 @@ func (s *HttpServer) Deploy(context string, h RouteHandler) {
 
 func (s *HttpServer) Start() error {
 	return http.ListenAndServe(fmt.Sprintf("%s:%s", s.address, s.port), nil)
+}
+
+type Error struct {
+	Msg string
+}
+
+func NewError(msg string) *Error {
+	s := &Error{Msg: msg}
+	return s
+}
+
+type Route struct {
+	Path       string
+	Method     string
+	HandleFunc http.HandlerFunc
+}
+
+func NewRoute(p string, m string, hf http.HandlerFunc) *Route {
+	r := &Route{Path: p, Method: m, HandleFunc: hf}
+	return r
+}
+
+func (r *Route) HasIncorrectHttpMethod(method string) bool {
+
+	if len(r.Method) != 0 && method != r.Method {
+		return true
+	}
+	return false
+
+}
+
+type RouteHandler interface {
+	GetRoutes() []*Route
 }
