a79bd750f536e70b2b7d3d540dc916200c470a1f
regiluze <regiluze@hotmail.com>
2015-02-22 01:27:00 +0100

    bugfix

--- server.go
+++ server.go
@@ -28,7 +28,7 @@ type HttpServer struct {
 }
 
 type HttpRouter interface {
-	HandleFunc(string, http.HandlerFunc)
+	HandleFunc(string, http.HandlerFunc) *mux.Route
 	Handle(string, http.Handler) *mux.Route
 	ServeHTTP(http.ResponseWriter, *http.Request)
 	NotFoundHandler() http.Handler
